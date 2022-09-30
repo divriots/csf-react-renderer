8ab959a0f6baaf0d61d391293eedb0b77e3ff189
Francois Valdy <francois.valdy@gmail.com>
2022-09-30 17:25:35 +0200

    Push from hooks (#5)
    Co-authored-by: Francois Valdy <604263+undefined@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import ReactDOM from 'react-dom';
+import React from 'react';
 export {render} from './render';
 
 export async function renderToDOM(
-  { storyFn },
+  {
+    storyContext,
+    unboundStoryFn,
+  },
   domElement
 ) {
   await new Promise<void>((resolve) => 
-    ReactDOM.render(storyFn(), domElement, () => resolve())
+    ReactDOM.render(React.createElement(unboundStoryFn, storyContext), domElement, () => resolve())
   );
   return () => ReactDOM.unmountComponentAtNode(domElement);
 }
